34b446313bb5ca0cf4f55687f4cbea3ae58eeaaf
notsatan <am_satan@protonmail.com>
2021-10-07 19:58:02 +0530

    Update: Convert `Codecov` action to be optional
    Add a field to allow users to opt-out of using Codecov if needed.
    Not using Codecov configs would ensure that the Codecov action
    to generate code coverage is not included in the final output of
    the template, and at the same time ensure that the `codecov.yml`
    file is also removed

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -2,20 +2,20 @@ from re import match
 from sys import exit
 from typing import Callable
 
-# Regex to determine if a module name is valid. Conditions to pass;
-#   - First letter should be lower case alphabet
-#   - Should consist of alphabets and/or hyphen ONLY
-#   - Should have length between [3, 11] characters (inclusive)
-MODULE_NAME_REGEX: str = r"^[a-z][a-zA-Z\-]{2,10}$"
-
-# Name of the module from Cookiecutter configs
-module_name = r"{{ cookiecutter.module_name }}"
-
 
 def validate_module_name():
     """
     Raises `ValueError` if module name is invalid. Uses `MODULE_NAME_REGEX` to validate
     """
+
+    # Regex to determine if a module name is valid. Conditions to pass;
+    #   - First letter should be lower case alphabet
+    #   - Should consist of alphabets and/or hyphen ONLY
+    #   - Should have length between [3, 11] characters (inclusive)
+    MODULE_NAME_REGEX: str = r"^[a-z][a-zA-Z\-]{2,10}$"
+
+    # Name of the module from Cookiecutter configs
+    module_name = r"{{ cookiecutter.module_name }}"
 
     if not match(MODULE_NAME_REGEX, module_name):
         raise ValueError(f"Error: Invalid module name: `{module_name}`")
