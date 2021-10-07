f79589341778268db974e78b64446749c4bcc89f
notsatan <am_satan@protonmail.com>
2021-10-07 20:42:24 +0530

    Update: Allow users to deny using Github actions in the Project
    If a user decides to not use Github workflows, the post hook
    script would delete the `.github` directory, additionally,
    would also delete the `codecov.yml` file since it won't be used
    outside the defined workflows

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,9 +2,10 @@
 
 from os import mkdir, remove
 from os.path import join
+from shutil import rmtree
 from pathlib import Path
 from sys import exit
-from typing import Callable, List
+from typing import Callable, List, Optional, Any
 
 # Contains path to the project directory - i.e. {{cookiecutter.project_name}}
 CUR_DIR = Path.cwd().absolute()
@@ -16,18 +17,6 @@ temp_directories: List[str] = [
     "coverage",
     "tmp",
 ]
-
-
-def remove_codecov():
-    """
-    Removes `codecov.yml` file from the project root if Codecov is not being used
-    """
-
-    codecov_needed: bool = bool("{{ cookiecutter.use_codecov }}".lower() == "y")
-
-    if not codecov_needed:
-        # If Codecov is not needed, delete `codecov.yml`
-        remove("codecov.yml")
 
 
 def create_temp_directories():
@@ -43,9 +32,39 @@ def create_temp_directories():
             pass
 
 
-runners: Callable[[], None] = [
+def remove_codecov(*, force: bool = False):
+    """
+    Removes `codecov.yml` file from the project root if Codecov is not being used
+    """
+
+    codecov_needed: bool = bool("{{ cookiecutter.use_codecov }}".lower() == "y")
+
+    if not codecov_needed or force:
+        # If Codecov is not needed, delete `codecov.yml`
+        remove("codecov.yml")
+        return True
+
+    return False
+
+
+def remove_workflows(*, force: bool = False):
+    """
+    Removes the `.github` directory containing Github workflows from project root if not needed
+    """
+
+    actions_needed: bool = bool("{{ cookiecutter.use_github_actions }}".lower() == "y")
+
+    if not actions_needed or force:
+        rmtree("./.github", ignore_errors=True)
+
+        # Since workflows are to be removed, remove `codecov.yml` as well
+        remove_codecov(force=True)
+
+
+runners: Callable[[Optional[Any]], None] = [
     create_temp_directories,
     remove_codecov,
+    remove_workflows,
 ]
 
 for runner in runners:
