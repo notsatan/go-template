638edbaab04d2d148532a122575f1fe06cd14fbc
notsatan <am_satan@protonmail.com>
2021-10-20 23:49:20 +0530

    Minor: Switch to using complete import paths
    The function names were becoming weird, with the common ones often
    being used up by an imported function.
    
    Decided to get rid of this and replace them with complete import
    paths for the common function

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,6 @@
-from os import mkdir, remove
-from os.path import join, exists
-from shutil import rmtree, move
+import os
+import shutil
+
 from pathlib import Path
 from sys import exit
 from typing import Callable, List, Optional, Any
@@ -22,10 +22,10 @@ def create_temp_directories():
     """
 
     for directoryName in temp_directories:
-        dir_path: str = join(CUR_DIR, directoryName)
-        mkdir(dir_path)
+        dir_path: str = os.path.join(CUR_DIR, directoryName)
+        os.mkdir(dir_path)
 
-        with open(join(dir_path, ".gitignore"), "w") as _:
+        with open(os.path.join(dir_path, ".gitignore"), "w") as _:
             pass
 
 
@@ -38,12 +38,12 @@ def remove_codecov(*, force: bool = False):
 
     if not codecov_needed or force:
         # If Codecov is not needed, delete `codecov.yml`
-        dest_path: str = join(CUR_DIR, ".codecov.yml")
+        dest_path: str = os.path.join(CUR_DIR, ".codecov.yml")
 
-        if not exists(dest_path):
+        if not os.path.exists(dest_path):
             return False  # The file to delete does not exist
 
-        remove(dest_path)
+        os.remove(dest_path)
         return True
 
     return False
@@ -57,11 +57,11 @@ def remove_workflows(*, force: bool = False):
     actions_needed: bool = bool("{{ cookiecutter.use_github_actions }}".lower() == "y")
 
     if not actions_needed or force:
-        dest_path: str = join(CUR_DIR, ".github")
-        if not exists(dest_path):
+        dest_path: str = os.path.join(CUR_DIR, ".github")
+        if not os.path.exists(dest_path):
             return False  # The directory to delete does not exist
 
-        rmtree(dest_path, ignore_errors=True)
+        shutil.rmtree(dest_path, ignore_errors=True)
 
         # Since workflows are to be removed, remove `codecov.yml` as well
         remove_codecov(force=True)
@@ -77,14 +77,14 @@ def precommit_handler():
     if use_precommit:
         return
 
-    config_file: str = join(CUR_DIR, ".pre-commit-config.yaml")
-    workflow: str = join(CUR_DIR, ".github", "workflows", "pre-commit.yml")
+    config_file: str = os.path.join(CUR_DIR, ".pre-commit-config.yaml")
+    workflow: str = os.path.join(CUR_DIR, ".github", "workflows", "pre-commit.yml")
 
-    if exists(config_file):
-        remove(config_file)
+    if os.path.exists(config_file):
+        os.remove(config_file)
 
-    if exists(workflow):
-        remove(workflow)
+    if os.path.exists(workflow):
+        os.remove(workflow)
 
 
 def lincese_generator():
@@ -97,12 +97,12 @@ def lincese_generator():
     selected_license: str = "{{ cookiecutter.license }}"
     license_file: str = selected_license.split()[0].upper()
 
-    source = join(CUR_DIR, "_licenses", license_file)
-    destination = join(CUR_DIR, "LICENSE")
+    source = os.path.join(CUR_DIR, "_licenses", license_file)
+    destination = os.path.join(CUR_DIR, "LICENSE")
 
     # Move the selected license file, remove the `_licenses` directory when done
-    move(source, destination)
-    rmtree(join(CUR_DIR, "_licenses"), ignore_errors=True)
+    shutil.move(source, destination)
+    shutil.rmtree(os.path.join(CUR_DIR, "_licenses"), ignore_errors=True)
 
 
 def remove_security_md():
@@ -116,7 +116,7 @@ def remove_security_md():
     if not should_remove:
         return
 
-    remove(join(CUR_DIR, "SECURITY.md"))
+    os.remove(os.path.join(CUR_DIR, "SECURITY.md"))
 
 
 runners: Callable[[Optional[Any]], None] = [
