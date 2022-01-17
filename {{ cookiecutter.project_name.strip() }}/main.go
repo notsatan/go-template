7ed24260ca8b59668f82a5e5eebb8c9f0429496c
notsatan <git@notsatan.me>
2022-01-17 23:14:12 +0530

    Minor: Add tests to cover all cases
    Projects generated using the prior build would fail Codecov
    analysis since the test coverage was too low - with this commit,
    tests should be back to covering 100% of the code-base!

--- {{ cookiecutter.project_name.strip() }}/main.go
+++ {{ cookiecutter.project_name.strip() }}/main.go
@@ -7,16 +7,6 @@ import (
 	"{{ cookiecutter.go_module_path.strip('/') }}/src"
 )
 
-// title contains the name of the project
-const title = "{{ cookiecutter.project_name.strip() }}"
-
-/*
-ProjectName returns the value of `title` string
-*/
-func ProjectName() string {
-	return title
-}
-
 func main() {
 	fmt.Printf("Running project: `%s`\n", src.ProjectName())
 
--- {{ cookiecutter.project_name.strip() }}/main_test.go
+++ {{ cookiecutter.project_name.strip() }}/main_test.go
@@ -1,13 +1,64 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
-func TestProjectName(t *testing.T) {
-	if ProjectName() != title {
-		t.Errorf("(/main) project title does not match")
-	}
+const (
+	envProd  = "production_mode"
+	envDebug = "debug_mode"
+	envBuild = "__BUILD_MODE__"
+)
+
+func resetEnv() {
+	// Unset all environment variables
+	os.Unsetenv(envProd)
+	os.Unsetenv(envDebug)
+	os.Unsetenv(envBuild)
+}
+
+func TestMain(m *testing.M) {
+	// Run all tests, unset env variables, and exit
+	resetEnv()
+	val := m.Run()
+	resetEnv()
+	os.Exit(val)
 }
 
 func TestMainMethod(_ *testing.T) {
 	main()
+}
+
+func dirtyCheck(t *testing.T, val01, val02 bool) {
+	t.Helper()
+
+	if val01 != val02 {
+		t.Errorf("Values don't match: (%v, %v)", val01, val02)
+	}
+}
+
+func TestFirst(t *testing.T) {
+	resetEnv()
+	dirtyCheck(t, firstCheck(), false) // no variable should be detected
+
+	os.Setenv(envProd, "production")
+	dirtyCheck(t, firstCheck(), true) // detect `production` mode
+
+	resetEnv()
+	os.Setenv(envDebug, "debug")
+	dirtyCheck(t, firstCheck(), true) // detect `debug` mode
+}
+
+func TestSecond(t *testing.T) {
+	resetEnv()
+	dirtyCheck(t, secondCheck(), false) // no variable should be detected
+
+	resetEnv()
+	os.Setenv(envBuild, "production")
+	dirtyCheck(t, secondCheck(), true) // detect `production` mode!
+
+	resetEnv()
+	os.Setenv(envBuild, "debug")
+	dirtyCheck(t, secondCheck(), true) // detect `debug` mode!
 }
